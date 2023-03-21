06883c940d211164c185aa89f3fcd94903813495
sousakak7 <sousakak7@gmail.com>
2023-03-21 19:50:38 +0900

    add usage

--- assets/scripts.js
+++ assets/scripts.js
@@ -6,20 +6,44 @@
 ██████╔╝╚█████╔╝██║░░██║██║██║░░░░░░░░██║░░░██████╔╝██╗╚█████╔╝██████╔╝
 ╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░░░░░░░╚═╝░░░╚═════╝░╚═╝░╚════╝░╚═════╝░
 **********************************************************************/
-$(".header-list-li").click(function() {
-    var index = jQuery(this).index()
-    var link = $(".header-list-li").eq(index).children("a").attr("href");
-    location.href = link;
-});
+$(function(){
+    $('.content-section').each(function() {
+        if ($(window).height() > $(this).offset().top + 200) {
+            $(this).addClass('loaded');
+        }
+    });
 
-$(".header-title-box").click(function() {
-    location.href = "/";
-});
+    $('.header-list-li').click(function() {
+        var index = jQuery(this).index()
+        var link = $('.header-list-li').eq(index).children('a').attr("href");
+        location.href = link;
+    });
 
-$(window).scroll(function () {
-    if($(window).scrollTop() > 20) {
-        $('header').css("position", "fixed");
-    } else {
-        $('header').css("position", "static");
-    }
+    $('.header-title-box').click(function() {
+        location.href = "/";
+    });
+
+    $('.ext-link1').click(function() {
+        // content
+    });
+
+    // when scrolled
+    $(window).scroll(function () {
+        // header
+        if ($(window).scrollTop() - $('main').offset().top > 50) {
+            $('header').css("position", "fixed");
+        } else {
+            $('header').css("position", "static");
+        }
+
+        // display content
+        $('.content-section').each(function() {
+            var pos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var height = $(window).height();
+            if (scroll > pos - height + 200) {
+                $(this).addClass('loaded');
+            }
+        });
+    });
 });
