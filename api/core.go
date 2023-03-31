912eb2daf16f8bfe2f4dd3224008a4759c9bbf6f
sousakak7 <sousakak7@gmail.com>
2023-03-31 17:23:48 +0900

    _

--- api/core.go
+++ api/core.go
@@ -1,12 +1,14 @@
 package api
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"reflect"
+	"strconv"
 
 	"github.com/joho/godotenv"
 
@@ -14,15 +16,21 @@ import (
 	"google.golang.org/api/sheets/v4"
 )
 
-type Data struct {
-	name        string `json:"name"`
-	witch_name  string `json:"witch-name"`
-	called_name string `json:"called-name"`
-	desc        string `json:"description"`
-	chap        string `json:"chapter"`
+type Params struct {
+	sheet string
+	char  string
+	elem  string
 }
 
-func Main() string {
+type Data struct {
+	Name        string `json:"name"`
+	Witch_name  string `json:"witch_name"`
+	Called_name string `json:"called_name"`
+	Desc        string `json:"desc"`
+	Chap        string `json:"chap"`
+}
+
+func Main(params Params) string {
 	err := godotenv.Load(".env")
 	if err != nil {
 		fmt.Printf("環境変数の読み込みに失敗しました: %v", err)
@@ -33,32 +41,133 @@ func Main() string {
 	if err != nil {
 		log.Fatal(err)
 	}
-	readRange := "イレイナ!A:E"
-	resp, err := srv.Spreadsheets.Values.Get(spreadsheetID, readRange).Do()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	if len(resp.Values) == 0 {
-		log.Fatalln("data not found")
-	}
 
 	raw_datas := []Data{}
-	for index, row := range resp.Values {
-		if index == 0 {
-			continue
+	var single_datas string
+	switch {
+	case params.char != "":
+		readRange := params.sheet + "!A:E"
+		resp, err := srv.Spreadsheets.Values.Get(spreadsheetID, readRange).MajorDimension("COLUMNS").Do()
+		if err != nil {
+			log.Fatalln(err)
 		}
-		for len(row) == 5 {
-			row = append(row, "")
+		if len(resp.Values) == 0 {
+			log.Fatalln("data not found")
 		}
-		single_data := Data{row[0].(string), row[1].(string), row[2].(string), row[3].(string), row[4].(string)}
-		raw_datas = append(raw_datas, single_data)
+
+		var rownum int = -1
+		for index, name := range resp.Values[0] {
+			if name.(string) == params.char {
+				rownum = index + 1
+				break
+			}
+		}
+		if rownum != -1 {
+			targetRow := params.sheet + "!A" + strconv.Itoa(rownum) + ":E" + strconv.Itoa(rownum)
+			rowresp, err := srv.Spreadsheets.Values.Get(spreadsheetID, targetRow).Do()
+			if err != nil {
+				log.Fatalln(err)
+			}
+			if len(rowresp.Values) == 0 {
+				log.Fatalln("data not found")
+			}
+			if params.elem != "" {
+				rtCstStruct := Data{Name: "", Witch_name: "", Called_name: "", Desc: "", Chap: ""}
+				rtCst := reflect.TypeOf(rtCstStruct)
+				var elemList = []string{}
+				for i := 0; i < rtCst.NumField(); i++ {
+					elemList = append(elemList, rtCst.Field(i).Name)
+				}
+				var elemIndex int = -1
+				for i, elemName := range elemList {
+					if elemName == params.elem {
+						elemIndex = i
+						break
+					} else {
+						continue
+					}
+				}
+				if elemIndex != -1 {
+					single_datas = "[\"" + rowresp.Values[0][elemIndex].(string) + "\"]"
+				}
+			} else {
+				v := rowresp.Values[0]
+				for len(v) < 5 {
+					v = append(v, "")
+				}
+				raw_datas = append(raw_datas, Data{Name: v[0].(string), Witch_name: v[1].(string), Called_name: v[2].(string), Desc: v[3].(string), Chap: v[4].(string)})
+			}
+		}
+	case params.char == "" && params.elem != "":
+		var single_datas string
+		rtCstStruct := Data{Name: "", Witch_name: "", Called_name: "", Desc: "", Chap: ""}
+		rtCst := reflect.TypeOf(rtCstStruct)
+		var elemList = []string{}
+		for i := 0; i < rtCst.NumField(); i++ {
+			elemList = append(elemList, rtCst.Field(i).Name)
+		}
+		var elemIndex int = -1
+		for i, elemName := range elemList {
+			if elemName == params.elem {
+				elemIndex = i
+				break
+			} else {
+				continue
+			}
+		}
+		target := params.sheet + "!"
+		if elemIndex != -1 {
+			target = target + "ABCDE"[(elemIndex):(elemIndex+1)] + ":" + "ABCDE"[(elemIndex):(elemIndex+1)]
+			resp, err := srv.Spreadsheets.Values.Get(spreadsheetID, target).MajorDimension("COLUMNS").Do()
+			if err != nil {
+				log.Fatalln(err)
+			}
+			if len(resp.Values) == 0 {
+				log.Fatalln("data not found")
+			}
+			single_datas = "["
+			for index, content := range resp.Values[0] {
+				if index == 0 {
+					continue
+				}
+				single_datas += "\"" + content.(string) + "\","
+			}
+			single_datas = single_datas[:len(single_datas)-1]
+			single_datas += "]"
+		} else {
+			err := errors.New("index out of range: jumped out from a number of elements")
+			fmt.Println(err.Error())
+		}
+		return single_datas
+	default:
+		readRange := params.sheet + "!A:E"
+		resp, err := srv.Spreadsheets.Values.Get(spreadsheetID, readRange).Do()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		if len(resp.Values) == 0 {
+			log.Fatalln("data not found")
+		}
+
+		for index, row := range resp.Values {
+			if index == 0 {
+				continue
+			}
+			for len(row) < 5 {
+				row = append(row, "")
+			}
+			single_data := Data{Name: row[0].(string), Witch_name: row[1].(string), Called_name: row[2].(string), Desc: row[3].(string), Chap: row[4].(string)}
+			raw_datas = append(raw_datas, single_data)
+		}
 	}
 
-	var buf bytes.Buffer
-	enc := json.NewEncoder(&buf)
-	if err := enc.Encode(&raw_datas); err != nil {
-		log.Fatal(err)
+	if single_datas != "" {
+		return single_datas
+	} else {
+		bytes, err := json.Marshal(raw_datas)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return string(bytes)
 	}
-
-	return buf.String()
 }
